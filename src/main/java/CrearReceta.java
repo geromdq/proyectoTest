dca6eb5180e4abd43a3abf7e02fef252fb8037bb
Gero <gero295@gmail.com>
2020-07-14 21:05:38 -0300

    First commit

---
+++ src/main/java/CrearReceta.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CrearReceta {
+
+
+    Scanner sc = new Scanner(System.in);
+    public String agregarIngrediente(){
+        System.out.println("Ingrese un ingrediente para la receta");
+        return (nextLine();
+    }
+
+}
---
+++ src/main/java/Receta.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class Receta {
+
+    ArrayList ingredientes;
+    ArrayList pasosReceta;
+
+
+    public ArrayList getIngredientes(){
+        return ingredientes;
+    }
+
+    public void setIngredientes(ArrayList ingredientes){
+        this.ingredientes = ingredientes;
+    }
+
+    public ArrayList getPasosReceta(){
+        return pasosReceta;
+    }
+
+    public void setPasosReceta(ArrayList pasosReceta){
+        this.pasosReceta = pasosReceta;
+    }
+
+
+
+
+
+
+
+
+}
