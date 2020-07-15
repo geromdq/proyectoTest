fe06aa9217067743a2900909ba819f9c01172daf
Gero <gero295@gmail.com>
2020-07-14 21:25:33 -0300

    nextline method added

--- src/main/java/CrearReceta.java
+++ src/main/java/CrearReceta.java
@@ -6,7 +6,12 @@ public class CrearReceta {
     Scanner sc = new Scanner(System.in);
     public String agregarIngrediente(){
         System.out.println("Ingrese un ingrediente para la receta");
-        return (nextLiness();
+        return (nextLines());
+    }
+
+    private String nextLines() {
+        System.out.println("a");
+        return "a";
     }
 
 }
